166792f280e7ffdbdba419209e9f5b5e29793587
Simon Marquis <contact@simon-marquis.fr>
2022-11-06 13:43:26 +0100

    Replace AndroidManifest.xml package with namespace

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ android {
     compileSdk = 33
     defaultConfig {
         applicationId = "fr.smarquis.sleeptimer"
+        namespace = "fr.smarquis.sleeptimer"
         minSdk = 26
         targetSdk = 33
         versionCode = versionMajor * 10000 + versionMinor * 1000 + versionPatch * 100 + versionBuild
