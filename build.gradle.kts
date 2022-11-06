907722bbb159f0f00847b9c36d98a0348d314b1e
Simon Marquis <contact@simon-marquis.fr>
2022-11-06 13:30:50 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
 
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.0-alpha02")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
     }
 }
 
