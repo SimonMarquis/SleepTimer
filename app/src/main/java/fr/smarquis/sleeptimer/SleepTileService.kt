8398fdb1af47b1abf8c96bf78181ea29d3681202
Simon Marquis <contact@simon-marquis.fr>
2023-06-13 22:08:13 +0200

    Fix API 34 deprecations

--- app/src/main/java/fr/smarquis/sleeptimer/SleepTileService.kt
+++ app/src/main/java/fr/smarquis/sleeptimer/SleepTileService.kt
@@ -1,10 +1,13 @@
 package fr.smarquis.sleeptimer
 
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.PendingIntent.getActivity
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.Q
+import android.os.Build.VERSION_CODES.TIRAMISU
 import android.provider.Settings
 import android.service.quicksettings.Tile.STATE_ACTIVE
 import android.service.quicksettings.Tile.STATE_INACTIVE
@@ -54,6 +57,9 @@ class SleepTileService : TileService() {
     private fun requestNotificationsPermission() = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
         addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
-    }.let(::startActivityAndCollapse)
+    }.let {
+        if (SDK_INT <= TIRAMISU) @Suppress("DEPRECATION") startActivityAndCollapse(it)
+        else startActivityAndCollapse(getActivity(this, 0, it, FLAG_IMMUTABLE))
+    }
 
 }
